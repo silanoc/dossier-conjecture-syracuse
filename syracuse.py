19601a70c45dbdd9c7afb185d82950ee880684b2
silanoc <silanoc@gmail.com>
2021-10-29 12:59:15 +0200

    correction orthographe, neutralisation à défaut de correction de vol en altitide pour plage de nb

--- syracuse.py
+++ syracuse.py
@@ -8,10 +8,10 @@ import matplotlib.pyplot as plt
 ####
 
 def transformation(nb_entree):
-	"""l'operation de base de la conjoncture de syracuse,
+	"""l'opération de base de la conjoncture de syracuse,
 
 	attrs:
-		- nb_entree(int) : le nombre a calculer
+		- nb_entree(int) : le nombre à calculer
 
 	returns:
 		- nb_sortie(int) : le nombre issu de la transformation
@@ -25,13 +25,13 @@ def transformation(nb_entree):
 
 
 def transformation_en_chaine(nb_depart):
-	""" pour un nombre donné, cherche l'intergralité de la suite jusqu'a se fin avec 1. utilse pour cela la fonction transformation()
+	""" pour un nombre donné, cherche l'intégralité de la suite jusqu'à la fin avec 1. Utilise pour cela la fonction transformation()
 
 	attrs:
-		- nb_depart(int) : le nombre a transfirmer de façon recursive
+		- nb_depart(int) : le nombre à transformer de façon récursive
 
 	returns:
-		- liste_de_la_suite(liste) : le nombre de depart puis tous ceux issus du calcul jusqu'à 1 inclus
+		- liste_de_la_suite(liste) : le nombre de départ puis tous ceux issus du calcul jusqu'à 1 inclus
 
 	"""
 	nb_travail = nb_depart
@@ -63,8 +63,8 @@ def chaine_en_serie(debut, fin):
 #####
 
 def analyse_un_vol(vol):
-	"""analyse les caractéristique d'un vol
-	le vocabulaire est issus de la page wikipédia
+	"""analyse les caractéristiques d'un vol.
+	le vocabulaire est issus de la page wikipédia.
 	
 	attrs:
 		- vol(chaine): la chaine à analyser
@@ -81,7 +81,7 @@ def analyse_un_vol(vol):
 			temps_de_vol_en_altitude+=1
 	temps_de_vol_en_altitude-=1 #je comprends pas pq, mais permet de retomber sur l'exemple de wikipedia
 
-	analyse_textuelle=f"temps de vol : {temps_de_vol} - altitude maximal : {altitude_maximal} - temsp de vol en atltitude : {temps_de_vol_en_altitude}"
+	analyse_textuelle=f"temps de vol : {temps_de_vol} - altitude maximale : {altitude_maximal} - temps de vol en atltitude : {temps_de_vol_en_altitude}"
 	analyse_totale=[analyse_textuelle, altitude_maximal, temps_de_vol, temps_de_vol_en_altitude]
 
 	return  analyse_totale
@@ -112,7 +112,7 @@ def analyse_multi_vol(liste_de_vol):
 	for i in range(0,len(liste_de_vol)):
 		liste_altitudes.append(max(liste_de_vol[i]))	
 		liste_temps_vol.append(len(liste_de_vol[i]))
-	#liste des vol en altitutes
+	#liste des vols en altitute
 	liste_vol_en_altitude=[]
 	for i in range(0,len(liste_de_vol)):
 		temps_de_vol_en_altitude=0
@@ -120,7 +120,7 @@ def analyse_multi_vol(liste_de_vol):
 			if liste_de_vol[i][j]>liste_de_vol[i][0]:
 				temps_de_vol_en_altitude+=1
 		#temps_de_vol_en_altitude-=1 #je comprends pas pq, mais permet de retomber sur l'exemple de wikipedia
-		#WARNING erreur sur ce calcul
+		#WARNING erreur sur ce calcul. Ca fonctionne très bien sur un seul nombre, mais pas sur une série !
 		liste_vol_en_altitude.append(temps_de_vol_en_altitude)
 	#analyse par chiffre et analyse textuelle par chiffre
 	analyse_par_chiffre=[]
@@ -128,7 +128,9 @@ def analyse_multi_vol(liste_de_vol):
 	for i in range(0,len(liste_de_vol)):
 		analyse_par_chiffre.append([liste_de_vol[i][0],liste_altitudes[i], liste_temps_vol[i], liste_vol_en_altitude[i]])
 		print(analyse_par_chiffre)
-		phrase=f"pour le nombre {analyse_par_chiffre[i][0]}, l'altitide max est {analyse_par_chiffre[i][1]}, le temps de vol est {analyse_par_chiffre[i][2]} et le vol en altitude vaut {analyse_par_chiffre[i][3]}"
+		# phrase supprimé en attendatn la correction
+		# phrase=f"pour le nombre {analyse_par_chiffre[i][0]}, l'altitude maximale est {analyse_par_chiffre[i][1]}, le temps de vol est {analyse_par_chiffre[i][2]} et le vol en altitude vaut {analyse_par_chiffre[i][3]}"
+		phrase=f"pour le nombre {analyse_par_chiffre[i][0]}, l'altitude maximale est {analyse_par_chiffre[i][1]}, le temps de vol est {analyse_par_chiffre[i][2]}"
 		liste_analyse_textuelle.append(phrase)
 	return analyse_par_chiffre, liste_analyse_textuelle
 	#plus grande altitude, temps de vol, vol en altitude
@@ -139,7 +141,7 @@ def graphique_multi_vol(liste_de_vol):
 	"""Fonction servant à afficher un graphique du vol pour plusieurs nombres
 	
 	attrs :
-		- vol(liste): liste de liste de chiffre, typiquement issus du calcul 
+		- vol(liste): liste de liste de chiffre, typiquement issue du calcul 
 
 	returns:
 		- pas de valeur, se contente d'afficher le graphique
@@ -147,15 +149,16 @@ def graphique_multi_vol(liste_de_vol):
 	fig, ax = plt.subplots()  # Create a figure and an axes.
 	for i in range(len(liste_de_vol)):
 		ax.plot(liste_de_vol[i], label=str(i))  # Plot some data on the axes.
-	ax.set_xlabel('x label')  # Add an x-label to the axes.
-	ax.set_ylabel('y label')  # Add a y-label to the axes.
-	ax.set_title("Simple Plot")  # Add a title to the axes.
-	ax.legend()  # Add a legend.
+	ax.set_xlabel('temps de vol')  # Add an x-label to the axes.
+	ax.set_ylabel('altitude')  # Add a y-label to the axes.
+	ax.set_title("graphique de vol pour une plage de  nombre")  # Add a title to the axes.
+	ax.legend("graphique de vol pour une plage de  nombre")  # Add a legend.
 	plt.show()
 
 
 ##################
-# quelques routines pour tester/expérimenter le code - peut être supprimées plus tard
+# quelques routines pour tester/expérimenter le code - peut être supprimé plus tard
+# pour le moment elle sont utilisé par l'interface textuelle
 ##################
 
 def testerjusteunetransformation():
@@ -163,7 +166,7 @@ def testerjusteunetransformation():
 	print(transformation(-17))
 
 def testertransformationenchaine():
-	a=(transformation_en_chaine(15))
+	a=(transformation_en_chaine(127))
 	print(a)
 	print(analyse_un_vol(a))
 	graphique_un_vol(a)
@@ -188,11 +191,11 @@ def choixinteractif():
 	Cela boucle sans fin jusqu'à demander à quitter le programme
 	"""
 	user_answer="a"
-	user_answer=input("Que voulez vous faire : \n tester transformation d'un nombre (tapez 1) \n toute la transformation d'un nombre (tapez 2)\n transformations pour une plage de nombre (tapez 3) \n ou quitter (tapez Q ou q) \n")
+	user_answer=input("Que voulez vous faire : \n la transformation d'un nombre (tapez 1) \n toute la transformation d'un nombre (tapez 2)\n transformations pour une plage de nombre (tapez 3) \n ou quitter (tapez Q ou q) \n")
 	while user_answer != "Q":
 		menu={"1":testerjusteunetransformation,"2":testertransformationenchaine, "3":testerchaineenserie, "q":fin, "Q":fin}
 		menu.get(user_answer,passer)()
-		user_answer=input("Que voulez vous faire : \n tester transformation d'un nombre (tapez 1) \n toute la transformation d'un nombre (tapez 2)\n transformations pour une plage de nombre (tapez 3)\n ou quitter (tapez Q ou q) \n")
+		user_answer=input("Que voulez vous faire : \n la transformation d'un nombre (tapez 1) \n toute la transformation d'un nombre (tapez 2)\n transformations pour une plage de nombre (tapez 3)\n ou quitter (tapez Q ou q) \n")
 
 def passer():
 	"""Pour gérer les réponses non-valides"""
@@ -209,8 +212,8 @@ def fin():
 
 def main():
 	"""pour exprimer ce qui va se passer"""
-	#choixinteractif()
-	testerchaineenserie()
+	choixinteractif()
+	#testerchaineenserie()
 
 if __name__ == "__main__":
 	main()
